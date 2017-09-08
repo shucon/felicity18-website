34184d825e62ffe659043a29dc40332ae0ec39e4
Megh Parikh <meghprkh@gmail.com>
2017-09-09 03:08:07 +0530

    Revamp placeholder site

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-				$('#fullpage').fullpage({
-					anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
-					menu: '#menu',
-					scrollingSpeed: 1000,
-					verticalCentered: false,
-					scrollOverflow: true
-				});
-			});
